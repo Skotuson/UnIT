db6ca5640650a8b2067422cae21cc6147fd14c07
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-04-13 12:24:41 +0200

    add window replace

--- createInventory.js
+++ createInventory.js
@@ -36,5 +36,7 @@ async function newInventory ( warehouseID ) {
         return
     }
     
-    localStorage.setItem("inventoryID", objJSON.winstrom.results[0].id );  
+    localStorage.setItem(`inventory${objJSON.winstrom.results[0].id}`, warehouseID );
+
+    window.location.replace(`/inventory.html?id=${objJSON.winstrom.results[0].id}`);
 }
