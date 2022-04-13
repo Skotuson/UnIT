2b447c96bd207f146e4a15eb915bceaf0e93f1aa
e_jd_m <adazalou@gmail.com>
2022-04-13 16:52:24 +0200

    feet

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 window.onload = async function () {
 
+  /*  if (!localStorage.getItem("basicAuth")) {
+     window.location.replace("/login");
+   } */
+
   const warehouses = await getWarehouses();
 
   //set options to select
@@ -8,10 +12,22 @@ window.onload = async function () {
   for (warehouse of warehouses) {
     warehouseSelect.innerHTML += `<option value="${warehouse.id}">${warehouse.nazev}</option>`;
   }
+
+  document.querySelector("#warehouse_select_update").innerHTML = warehouseSelect.innerHTML;
 };
 
 document.querySelector("#warehouse_select_form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const warehouseId = document.querySelector("#warehouse_select").value;
   newInventory(warehouseId);
+});
+
+
+document.querySelector("#warehouse_select_form_update").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const warehouseId = document.querySelector("#warehouse_select_update").value;
+  const invetories = getInventories(warehouseId);
+  $("#warehouse_update").modal('hide');
+  await sleep(1000);
+  $('#warhouse_update2').modal();
 });
