8854125f3d94ad2e3ef69ac9c07f35ce88317489
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-04-13 11:06:51 +0200

    initial commit

---
+++ createInventory.js
@@ -0,0 +1,40 @@
+
+async function newInventory ( warehouseID ) {
+    let url = `https://inventura.flexibee.eu/v2/c/firma3/inventura/`
+
+    const response = await fetch ( url, {
+        method: 'POST',
+        headers: {
+            'accept':'application/json',
+            'Authorization':'Basic YWRtaW4zOmFkbWluM2FkbWluMw==',
+            'Content-Type':'application/json'
+        },
+
+        body: JSON.stringify({
+            "winstrom": {
+              "inventura": [
+                {
+                  "popisInventury": "User description (This one without ID create record)",
+                  "typInventury": "Testovací inventura",
+                  "datZahaj": `${new Date().toISOString().slice(0,10)}`,
+                  "sklad": `${warehouseID}`,
+                  "stavK": "stavInventury.zahajena"
+                }
+              ]
+            }
+          })
+    } );
+
+    if ( ! response.ok ) {
+        alert ("Inventar ne");
+        return;
+    }
+
+    const objJSON = await response.json(); 
+    if ( ! objJSON.winstrom.stats.created ) {
+        alert ( "Created je 0!" );
+        return
+    }
+    
+    localStorage.setItem("inventoryID", objJSON.winstrom.results[0].id );  
+}
