5e9f32a467f4cd9cab3e513a72e7a5e2118e6d07
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-04-13 09:41:10 +0200

    initial commit

---
+++ insert.js
@@ -0,0 +1,18 @@
+let ean,
+    inventory     = 1,
+    warehouse     = 4,
+    pricing       = 0,
+    billinRange   = "2022",
+    wareCard      = 0;
+
+/**
+ * 
+ */
+function getItem ( warehouse, billingRange, ean ) {
+
+}
+
+function addToInventory ( inventory, warehouse, pricing, wareCard, count ) {
+    
+}
+
