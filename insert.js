b2458f5f9c2dbf913dae0ab0d045121f3bbb8230
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-04-13 09:51:52 +0200

    getItem function impl update

--- insert.js
+++ insert.js
@@ -2,17 +2,25 @@ let ean,
     inventory     = 1,
     warehouse     = 4,
     pricing       = 0,
-    billinRange   = "2022",
+    billingRange  = "2022",
     wareCard      = 0;
 
 /**
  * 
  */
-function getItem ( warehouse, billingRange, ean ) {
-
+async function getItem ( warehouse, billingRange, ean ) {
+    let url = "https://inventura.flexibee.eu/v2/c/firma3/skladova-karta/%28sklad%20%3D%20%22code%3ASKLAD%22%20and%20ucetObdobi%20%3D%20%22code%3A2022%22%20and%20cenik%3D%22ean%3A10001%22%29?detail=full";
+    const response = await fetch ( url, {
+        method: 'GET',
+        headers: {
+            'accept':'application/json',
+            'Authorization':'Basic YWRtaW4zOmFkbWluM2FkbWluMw=='
+        }
+    } );
+    console.log(response);
 }
 
 function addToInventory ( inventory, warehouse, pricing, wareCard, count ) {
-    
+
 }
 
